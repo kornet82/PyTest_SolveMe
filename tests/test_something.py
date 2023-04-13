f46cb134f98759dff00c40059f30c4b8854968e7
kornet82 <azarovp@gmail.com>
2023-04-14 01:41:45 +0200

    Add requests, src-enums-GlobalErrorMessages and 2 asserts

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,5 +1,13 @@
+import requests
+from configuration import SERVICE_URL
+from src.enums.global_enums import GlobalErrorMessages
+
+def test_getting_posts():
+    response = requests.get(url=SERVICE_URL)
+    received_posts = response.json()
+    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+    assert len(received_posts) == 2, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
 
 
 
-def test_equal():
-    assert 1 == 1, "Number is not equal to expected"
+
